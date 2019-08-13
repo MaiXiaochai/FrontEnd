017ebb6a3b531c4b476b66c83be07e5e93f34dbf
MaiXiaochai <maixiaochai@qq.com>
2019-08-13 18:07:45 +0800

    +)Angular

---
+++ Web/Angular/auction/src/app/app.component.ts
@@ -0,0 +1,16 @@
+// 从 angular的core组件中引入Component装饰器
+import { Component } from '@angular/core';
+
+// 用Component装饰器定义一个组件
+// 下边的代码块叫做组件源数据装饰器
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+// 普通的TypeScript类，告诉Angular AppComponent是一个组件，
+// 要把源数据附加到该类上
+// 整体理解，通过Component装饰器将源数据附加到 AppComponent类上
+export class AppComponent {
+  title = 'auction';
+}
