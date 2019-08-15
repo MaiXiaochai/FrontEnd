34b8fbbeebdf7816b71be78cb208c2ee52459c14
MaiXiaochai <maixiaochai@qq.com>
2019-08-15 18:01:08 +0800

    +)ActivatedRoute

--- Web/Angular/auction/src/app/stars/stars.component.ts
+++ Web/Angular/auction/src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
-
+  @Input()
+  private rating = 0;
+  private stars: boolean[];
   constructor() { }
 
   ngOnInit() {
+    // 这里通过用分数决定显示几个星星
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i <= this.rating);
+    }
+
   }
 
 }
