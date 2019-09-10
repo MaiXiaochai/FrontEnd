89dd9cb9e5f665a24fb3b7a047f3c09e7820fcee
MaiXiaochai <maixiaochai@qq.com>
2019-09-10 22:07:45 +0800

    +)AfterViewInit，AfterViewChecked

--- Web/Angular/demo/src/app/app.component.ts
+++ Web/Angular/demo/src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import {AfterContentChecked, AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {Child2Component} from './child2/child2.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  greeting = 'Hello';
-  user: {name: string} = {name: 'Tom'};
-  constructor() {
+export class AppComponent implements  OnInit, AfterViewInit, AfterViewChecked {
+  constructor() {}
+  // @ts-ignore
+  @ViewChild('child11')
+  child1: Child2Component;
+
+  ngAfterViewInit(): void {
+    console.log('父组件的视图初始化完毕');
+  }
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngAfterViewChecked(): void {
+    console.log('父组件的视图变更检测完毕');
+  }
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit(): void {
+    setInterval(() => {
+      this.child1.greeting('Tom');
+      }, 5000);
 
   }
+
 }
